38c3ab8defa538352c74a4b496c4ac197bf2806f
Joe Dalton <joseph.dalton@charter.com>
2021-08-20 13:23:02 -0600

    Initial Commit: Provided files

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p class="has-text-centered p-5">[Remove this element and make exercise code appear here.]</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
